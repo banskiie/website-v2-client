0c146831020f282f45e2717d84f30d38d4795786
Ivan <ivansinohon28@gmail.com>
2026-03-13 10:15:29 +0800

    test

--- conf/env.ts
+++ conf/env.ts
@@ -3,8 +3,6 @@ import { config } from "dotenv"
 config({ path: ".env" })
 
 export const {
-  NEXT_PUBLIC_GRAPHQL_HTTPS_URI,
-  NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_WSS_URI,
   NEXT_PUBLIC_JWT_ACCESS_SECRET,
   NEXT_PUBLIC_GRAPHQL_URI,
   NEXT_PUBLIC_GRAPHQL_WS_URI,
